0c67f635ca6ed7af41720d646b701e1a4588d9d7
kevinArnoldRamirez <64047619+kevinArnoldRamirez@users.noreply.github.com>
2020-06-01 12:21:09 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/arnold0824/archivostxt/MainActivity.java
@@ -0,0 +1,16 @@
+package mx.edu.isc.tesoem.arnold0824.archivostxt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.archivostxt.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
